184879fcdb206b70d62c4ad76e6f576325cfd892
SilasDias <silasdjd@gmail.com>
2019-07-29 23:15:02 -0300

    Desafio Pronto

---
+++ turma.rb
@@ -0,0 +1,21 @@
+class Turma
+  attr_reader :alunos
+  attr_accessor :nome
+
+  def initialize(nome)
+    @nome = nome
+    @alunos = []
+  end
+
+  def adiciona_aluno(aluno)
+  alunos << aluno
+  end
+
+  def total_alunos
+  puts "#{alunos.length()}"
+  end
+
+  private
+  attr_writer :alunos
+
+end
