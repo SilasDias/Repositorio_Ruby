cf3a4a026ebd85bc82d250f9fa0bcf2d90d6343f
SilasDias <silasdjd@gmail.com>
2019-11-08 16:37:47 -0300

    Concluido

---
+++ funcionario.rb
@@ -0,0 +1,14 @@
+class Funcionario
+  attr_accessor :nome, :ferias, :codigo_funcionario
+
+  def initialize(nome, codigo_funcionario)
+    @nome = nome
+    @codigo_funcionario = codigo_funcionario
+    @ferias = false
+  end
+
+  def imprime
+    puts "Funcionario #{nome} - Código #{codigo_funcionario} - Férias: #{ferias}"
+  end
+
+end
